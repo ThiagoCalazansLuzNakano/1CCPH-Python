ae852d7246dbb4d662bde380c8fb5998b22b2fed
tclnakano@gmail.com <labsfiap@fiap.com.br>
2026-03-17 09:42:19 -0300

    Aula-2-Variáveis

---
+++ Python-02/Desafios.py
@@ -0,0 +1,16 @@
+# Desafio 1
+nome = input("Digite o seu nome")
+print(f"Seja bem vindo {nome}!!")
+
+# Desafio 2
+dia = int(input("Digite seu dia de nascimento"))
+mês = int(input("Digite seu mês de nascimento"))
+ano = int(input("Digite o ano de seu nascimento"))
+
+print(f"{dia}/{mês}/{ano}")
+
+# Desafio 3
+numero_1 = int(input("Digite um numero"))
+numero_2 = int(input("Digite mais um numero"))
+
+print(numero)
---
+++ Python-02/Python-variaveis.py
@@ -0,0 +1,28 @@
+print("Ola mundo")
+
+print(7+4)
+print("7"+"4")
+print("7 + 4")
+
+# Comentários em python (1 linha)
+
+'''
+   Comentários
+   de multiplas 
+   linhas
+'''
+
+# VARIÁVEIS
+nome = "Thiago" # str
+idade = 17 # int
+peso = 67.8 #float
+
+print(nome, idade, peso)
+
+# INPUT - SIMULA FORMS NO CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = int(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+print(f"OI, {nome}!")
